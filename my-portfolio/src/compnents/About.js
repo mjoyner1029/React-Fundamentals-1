62a345db79e28ce868d2df573d4548179b04816d
Miles Joyner <62813978+mjoyner1029@users.noreply.github.com>
2024-09-30 11:59:36 -0700

    Update About.js

--- my-portfolio/src/compnents/About.js
+++ my-portfolio/src/compnents/About.js
@@ -4,7 +4,8 @@ const About = () => {
     return (
         <div>
             <h2>About Me</h2>
-            <p>This is the about section. You can write about your background, skills, and interests here.</p>
+            <p> I'm Miles Joyner from Oakland, California. Growing up playing football in East Oakland, I learned the value of teamwork and dedication. With a background influenced by my family—my father as a police officer, my mother in real estate, and my sister as a lawyer—I’m driven to make my mark in the tech industry. Passionate about technology and innovation, I aim to break barriers and empower others while shaping a bright future.
+      </p>
         </div>
     );
 };

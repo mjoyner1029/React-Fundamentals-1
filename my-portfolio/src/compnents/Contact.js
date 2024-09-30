36fe0ab05d0077909dd7fb529a8caf73fb8da07c
Miles Joyner <62813978+mjoyner1029@users.noreply.github.com>
2024-09-30 12:00:46 -0700

    Update Contact.js

--- my-portfolio/src/compnents/Contact.js
+++ my-portfolio/src/compnents/Contact.js
@@ -3,8 +3,9 @@ import React from 'react';
 const Contact = () => {
     return (
         <div>
-            <h2>Contact Me</h2>
-            <p>This is the contact section. You can provide your email, social media links, or a contact form here.</p>
+            <h2>Contact Information</h2>
+            <p>Email: <a href="mailto:Milesjoyner18@gmail.com">Milesjoyner18@gmail.com</a></p>
+            <p>LinkedIn: <a href="https://www.linkedin.com/in/miles-joyner-69a2252a8/" target="_blank" rel="noopener noreferrer">Miles Joyner</a></p>
         </div>
     );
 };
